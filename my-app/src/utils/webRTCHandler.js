9d91f87eb461d53f814736e2e60fa092444ccc70
Rehane <abusayid693@gmail.com>
2023-01-05 05:45:26 +0530

    feat: view local stream

--- my-app/src/utils/webRTCHandler.js
+++ my-app/src/utils/webRTCHandler.js
@@ -30,7 +30,6 @@ export const getLocalPreviewAndInitRoomConnection = (
     );
 };
 
-const showLocalVideoPreview = stream => {};
 
 let peers = {};
 let streams = [];
@@ -54,12 +53,48 @@ export const prepareNewPeerConnection = (connUserSockedId, isInitiator) => {
     stream: localStream
   });
 
+  peers[connUserSockedId].on('signal', data => {
+    // sdp, ice candidates
+    const signalData = {
+      signal: data,
+      connUserSockedId
+    };
+
+    wss.signalPeerData(signalData);
+  });
+
   peers[connUserSockedId].on('stream', stream => {
     console.log('new stream came');
 
     addStream(stream, connUserSockedId);
     streams = [...streams, stream];
   });
+};
+
+export const handleSignalingData = data => {
+  // save signaling data peer from other user
+  const {signal, connUserSockedId} = data;
+  peers[connUserSockedId].signal(signal)
+};
+
+//ui
+
+const showLocalVideoPreview = stream => {
+  const videosContainer = document.getElementById("videos_portal");
+  videosContainer.classList.add("videos_portal_styles");
+  const videoContainer = document.createElement('div');
+  videoContainer.classList.add('video_track_container')
+  const videoElement = document.createElement('video');
+  videoElement.autoplay = true
+  videoElement.muted = true;
+  videoElement.srcObject = stream;
+
+  videoElement.onloadedmetadata = ()=>{
+    videoElement.play()
+  }
+
+  videoContainer.appendChild(videoElement);
+  videosContainer.appendChild(videoContainer)
 };
 
 const addStream = () => {
